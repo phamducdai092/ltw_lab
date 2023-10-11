242a5a2c96007be7361f1683f8f5d55dee0baab2
Pham Duc Dai <phducdai47@gmail.com>
2023-10-11 21:30:10 +0700

    update page c and add lab 4.1

---
+++ lab_4_1/script.js
@@ -0,0 +1,18 @@
+let bgBlue = document.querySelector('.blue');
+let bgRed = document.querySelector('.red');
+let bgYellow = document.querySelector('.yellow');
+
+bgBlue.addEventListener('click', function bgBlue() {
+    document.body.style.backgroundColor = 'blue';
+});
+bgRed.addEventListener('click', function bgRed() {
+    document.body.style.backgroundColor = 'red';
+});
+bgYellow.addEventListener('click', function bgYellow() {
+    document.body.style.backgroundColor = 'yellow';
+});
+
+
+
+
+
